9c1d0f8d7b00b8809efdc99308ade90e0f878426
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-20 17:49:15 +0100

    changes to test

--- tests/test_run_morphographs.py
+++ tests/test_run_morphographs.py
@@ -5,8 +5,9 @@ from os.path import exists
 
 from src.morphologist_graphs_generator import run_morpho_graphs
 
+path: str = "/my/path/example/"
+
 def test_does_path_exists ():
-    path: str = "/my/path/example/"
 
     try:
         run_morpho_graphs(path)
@@ -15,9 +16,19 @@ def test_does_path_exists ():
         assert isinstance(e, FileNotFoundError) == True
         assert  "[Errno 2] No such file or directory: '/my/path/example/" in str(e) 
 
+    try: 
+        run_morpho_graphs(".", path)
+        assert 0 != 0
+    except FileNotFoundError as e:
+        assert isinstance(e, FileNotFoundError) == True
+        assert  "[Errno 2] No such file or directory: '/my/path/example/" in str(e) 
+
 def test_does_morphologist_create_outputs():
-    path: str = "/my/path/example/"
 
     run_morpho_graphs(path)
 
-    assert exists(f"{getcwd()}/../../runs/derivatives/morphologist-5.2/")
+    assert exists(f"{getcwd()}/../../derivatives/morphologist-5.2/")
+
+# def test_are_output_in_correct_location ():
+
+#     run_morpho_graphs()
