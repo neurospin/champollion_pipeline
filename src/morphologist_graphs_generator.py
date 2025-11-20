055d76731c7631aff166260e22f8c9b3ae6fe118
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-20 17:15:33 +0100

    raising filenotfounderror in the right case

--- src/morphologist_graphs_generator.py
+++ src/morphologist_graphs_generator.py
@@ -1,9 +1,15 @@
+import errno
+
 from os import getcwd
+from os import strerror
 from os.path import exists
 from argparse import ArgumentParser
 
 def run_morpho_graphs(input_path: str):
-    return input_path
+    if not exists(input_path):
+        raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), input_path)
+    else:
+        return input_path
 
 def main() -> None:
 
@@ -14,7 +20,7 @@ def main() -> None:
         )
     
     parser.add_argument("input", help="Path to the user's raw data.")
-    parser.add_argument("--output-dir", help="Path to the generated graphs from morphologist.")
+    parser.add_argument("--output-dir", help="Path to the generated graphs from morphologist. Default is the parent directory")
     parser.add_argument("--morphologist-path", help="Path to Morphologist.")
 
     args = parser.parse_args()
