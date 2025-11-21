4b0dc7fa3a192000cd143588d5e7435e3748c425
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-21 10:40:27 +0100

    implementing path tests on morphologist_graphs_generator

--- tests/test_run_morphographs.py
+++ tests/test_run_morphographs.py
@@ -9,19 +9,21 @@ path: str = "/my/path/example/"
 
 def test_does_path_exists ():
 
+    # Testing input path
     try:
         run_morpho_graphs(path)
-        assert 0 != 0
+        assert 0 != 0 # made to fail even if path exists
     except FileNotFoundError as e:
         assert isinstance(e, FileNotFoundError) == True
         assert  "[Errno 2] No such file or directory: '/my/path/example/" in str(e) 
 
+    # Testing output path
     try: 
         run_morpho_graphs(".", path)
         assert 0 != 0
     except FileNotFoundError as e:
         assert isinstance(e, FileNotFoundError) == True
-        assert  "[Errno 2] No such file or directory: '/my/path/example/" in str(e) 
+        assert "[Errno 2] No such file or directory: '/my/path/example/" in str(e) 
 
 def test_does_morphologist_create_outputs():
 
@@ -29,6 +31,13 @@ def test_does_morphologist_create_outputs():
 
     assert exists(f"{getcwd()}/../../derivatives/morphologist-5.2/")
 
-# def test_are_output_in_correct_location ():
+def test_are_output_in_correct_location ():
 
-#     run_morpho_graphs()
+    # Using local data for testing, need to adapt to your environment
+    local_input = f"{getcwd()}/../../runs/TEST_TEMPLATE/rawdata/"
+    local_output = f"{getcwd()}/../../runs/TEST08/derivatives/"
+    
+    #calling with local paths
+    run_morpho_graphs(local_input, local_output)
+
+    assert exists(f"{local_output}/morphologist-5.2/")
