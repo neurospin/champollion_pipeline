a2d5bf35946b6d7bd0ccf17b4a325712beaecbd2
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-21 16:07:46 +0100

    morphologist script first final form

--- src/morphologist_graphs_generator.py
+++ src/morphologist_graphs_generator.py
@@ -2,21 +2,44 @@ import errno
 
 from os import getcwd
 from os import strerror
+from os import listdir
+from os import chdir
 from os.path import exists
+from os.path import isfile
+from os.path import join
+from os.path import splitext
+from os.path import basename
 
 from subprocess import run
 
 from argparse import ArgumentParser
 
-def run_morpho_graphs(input_path: str):
+def run_morpho_graphs(input_path: str, output_path="./derivatives/"):
+    
     if not exists(input_path):
         raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), input_path)
+    if not exists(output_path):
+        raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), output_path)
     else:
-        return input_path
+        # List of allowed extensions for files as raw data for the pipeline
+        LIST_OF_EXTENSIONS: list[str] = [".nii.gz", ".nii", ".gz"]
+
+        input_files: list[str] = [
+            f for f in listdir(input_path) 
+                                  if isfile(join(input_path, f)) 
+                                  and splitext(basename(f))[1] in LIST_OF_EXTENSIONS
+                                  ]
+        
+        # print(f"morphologist_graphs_generator/run_morpho_graphs/list of files in string: {' '.join(input_files)}")
+
+        local_dir: str = getcwd()
+        chdir(input_path)
+        run(f"morphologist-cli {' '.join(input_files)} {output_path} -- --of morphologist-auto-nonoverlap-1.0", shell=True, executable="/bin/bash")
+        chdir(local_dir)
 
 def main() -> None:
 
-    print(f"Current working directory: {getcwd()}")
+    # print(f"Current working directory: {getcwd()}")
     parser: ArgumentParser = ArgumentParser(
         prog="morphologist_graphs_generator",
         description="Generating graphs with morphologist from the user raw data."
@@ -24,11 +47,11 @@ def main() -> None:
     
     parser.add_argument("input", help="Path to the user's raw data.")
     parser.add_argument("--output-dir", help="Path to the generated graphs from morphologist. Default is the parent directory")
-    parser.add_argument("--morphologist-path", help="Path to Morphologist.")
 
     args = parser.parse_args()
 
-    print(f"morphologist_graphs_generator.py/main/args/input: {(args.input)}")
+    # print(f"morphologist_graphs_generator.py/main/args/input: {(args.input)}")
+    run_morpho_graphs(args.input, args.output_dir)
 
 
 if __name__ == "__main__":
