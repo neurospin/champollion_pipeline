48120165a0089a05ed1a9c9cbee1b91270563eba
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-21 16:13:26 +0100

    Cleaning script

--- src/morphologist_graphs_generator.py
+++ src/morphologist_graphs_generator.py
@@ -14,7 +14,7 @@ from subprocess import run
 
 from argparse import ArgumentParser
 
-def run_morpho_graphs(input_path: str, output_path="./derivatives/"):
+def run_morpho_graphs(input_path: str, output_path):
     
     if not exists(input_path):
         raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), input_path)
@@ -29,8 +29,6 @@ def run_morpho_graphs(input_path: str, output_path="./derivatives/"):
                                   if isfile(join(input_path, f)) 
                                   and splitext(basename(f))[1] in LIST_OF_EXTENSIONS
                                   ]
-        
-        # print(f"morphologist_graphs_generator/run_morpho_graphs/list of files in string: {' '.join(input_files)}")
 
         local_dir: str = getcwd()
         chdir(input_path)
@@ -39,18 +37,17 @@ def run_morpho_graphs(input_path: str, output_path="./derivatives/"):
 
 def main() -> None:
 
-    # print(f"Current working directory: {getcwd()}")
+    print(f"Current working directory: {getcwd()}")
     parser: ArgumentParser = ArgumentParser(
         prog="morphologist_graphs_generator",
         description="Generating graphs with morphologist from the user raw data."
         )
     
     parser.add_argument("input", help="Path to the user's raw data.")
-    parser.add_argument("--output-dir", help="Path to the generated graphs from morphologist. Default is the parent directory")
+    parser.add_argument("output-dir", help="Path to the generated graphs from morphologist. Default is the parent directory")
 
     args = parser.parse_args()
 
-    # print(f"morphologist_graphs_generator.py/main/args/input: {(args.input)}")
     run_morpho_graphs(args.input, args.output_dir)
 
 
