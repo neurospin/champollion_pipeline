38d2dcec0a5b5e02d2a3390bc73d59dd1d2e3e09
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-25 16:15:12 +0100

    fixing refactor

--- src/utils/lib.py
+++ src/utils/lib.py
@@ -3,14 +3,13 @@ import errno
 from os import strerror
 from os.path import exists
 
-def does_folder_exists_or_except(file: str) -> None:
-     if not exists(file):
-        raise FileNotFoundError(errno.ENOENT, strerror(errno.ENOENT), file)
-     
-
-def are_paths_valid_or_except(paths: list[str]) -> None:
-   try: 
-      for p in paths:
-          does_folder_exists_or_except(p)
-   except FileNotFoundError as e:
-       raise e
+def are_paths_valid(paths: list[str]) -> bool | None:
+   if len(paths) <= 0:
+        raise ValueError("List doesn't contain any path.")
+   else:
+      index: int = 0
+      is_valid = True
+      while is_valid and index < len(paths):
+         is_valid = exists(paths[index])
+      
+      return is_valid
