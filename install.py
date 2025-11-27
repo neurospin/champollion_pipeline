6ce239db793167ed0dda6df75ad6af510b2c51f6
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-11-27 17:51:17 +0100

    creating installation and main script

---
+++ install.py
@@ -0,0 +1,69 @@
+from subprocess import run
+from subprocess import CalledProcessError
+
+from sys import stderr
+
+from os import getcwd
+from os import chdir
+from os.path import join
+
+from argparse import ArgumentParser
+
+def is_pixi_installed():
+    try:
+        result = run(
+            ["pixi", "--version"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        return True
+    except (CalledProcessError, FileNotFoundError):
+        return False
+
+def main(installation_dir: str) -> None:
+
+    local_dir: str = getcwd()
+    chdir(installation_dir)
+
+    link_to_deep_folding_repo: str = "https://github.com/neurospin/deep_folding.git"
+    link_to_champollion_repo: str = "https://github.com/neurospin/champollion_V1.git"
+
+    # Pixi part
+    if not is_pixi_installed():
+        print("Installing pixi: ")
+        run("curl -fsSL https://pixi.sh/install.sh | bash", shell=True, executable="/bin/bash")
+
+    run("pixi init -c https://brainvisa.info/neuro-forge -c pytorch -c nvidia -c conda-forge", shell=True, executable="/bin/bash")
+    with open("pixi.toml", mode="a") as conf:
+        conf.write('soma-env = ">=0.0"')
+        conf.write('libjpeg-turbo = {channel= "conda-forge", version= ">=3.0"}')
+        conf.write('')
+        conf.write('[pypi-dependencies]')
+        conf.write('dracopy = ">=1.4.2"')
+    
+    run("pixi add anatomist morphologist soma-env=0.0 pip", shell=True, executable="/bin/bash")
+
+    #Git part
+    run(f"git clone {link_to_deep_folding_repo}")
+    run(f"git clone {link_to_champollion_repo}")
+
+    #software installation part
+    chdir(join(installation_dir, 'deep_folding'))
+    run("SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True pip3 install -e .", shell=True, executable="/bin/bash")
+    chdir(join(installation_dir, "champollion_V1"))
+    run("pip3 install -e .")
+
+    return None
+
+if __name__ == "__main__":
+    parser: ArgumentParser = ArgumentParser(
+        prog=__file__,
+        description="Installing the different dependencies of the Champllion V1 pipeline."
+        )
+    
+    parser.add_argument("--installation_dir", help="Absolute path to the wished installation location", type=str, default=".")
+
+    args = parser.parse_args()
+    
+    main(args.installation_dir)
