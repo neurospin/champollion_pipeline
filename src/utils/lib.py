d4b65ed05a4ab3e4ecc1e7348067d6f1aab56088
Barthelemy Drabczuk <barthelemy.drabczuk@cea.fr>
2025-12-09 11:38:51 +0100

    fixing champollion config files generation

--- src/utils/lib.py
+++ src/utils/lib.py
@@ -14,3 +14,26 @@ def are_paths_valid(paths: list[str]) -> bool | None:
          index += 1
       
       return is_valid
+   
+
+def get_nth_parent_dir(folder: str, n: int) -> str:
+    """
+    Returns the absolute path to the nth parent of a directory
+    
+    :param folder: absolute path to a directory
+    :type folder: str
+    :param n: number of iteration to do
+    :type n: int
+    :return: absolute path to the nth parent
+    :rtype: str
+    """
+
+    # Substracting 3 instead of 1 because of the first and last element being ''
+    if n >= len(folder.split('/')) - 3:
+        return '/' + folder.split('/')[1] + '/'
+    else :
+        parent_folder: str = folder
+        for i in range(n) :
+            parent_folder = os.path.dirname(parent_folder)
+
+    return parent_folder
